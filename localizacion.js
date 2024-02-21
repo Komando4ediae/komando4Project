87a3f62d2a8f1bb755bdbb4a4187271b35795903
Vicky Tínar <159806456+VickyTinar@users.noreply.github.com>
2024-02-21 20:35:56 +0100

    Addlocalizacion.js

---
+++ localizacion.js
@@ -0,0 +1,7 @@
+const localizacionAlbarracin = {
+    latitud : 40.36,
+    longitud : -1.54,
+  };
+
+  /* Nosotros necesitamos modelo de datos con gps para la localización
+   para saber donde esta el sitio exacto para poder escalar */
