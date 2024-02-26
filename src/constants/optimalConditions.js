d39a771ee173386503ba347acc930c3f718e75d5
dinho-dev <68518134+dinho-dev@users.noreply.github.com>
2024-02-26 17:54:18 +0100

    improved code readability, removed unnecessary comments

---
+++ src/constants/optimalConditions.js
@@ -0,0 +1,18 @@
+export const optimalConditions = {
+    temperatureCelcius: {
+      min: 5,
+      max: 12
+    },
+    humidityPercentage: {
+      min: 0,
+      max: 60
+    },
+    windSpeed: {
+      min: 0,
+      max: 25
+    },
+    precipitationAmount: {
+      min: 0,
+      max: 0
+    }
+  };
