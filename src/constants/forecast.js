1f4d8aa19ce35ac96d9df66f6c08663f2d610fc9
Carlos ALvarez <carlosmail1978@gmail.com>
2024-02-27 17:52:16 +0100

    Change pronostico file to forecast.js and changes its atribbutes

---
+++ src/constants/forecast.js
@@ -0,0 +1,6 @@
+const forecast = {
+  temperatureCelcius: 11,
+  humidityPercentage: 50,
+  windSpeed: 5,
+  precipitationAmount: 0,
+};
--- src/constants/pronostico.js
+++
@@ -1,9 +0,0 @@
-const pronostico = {
-  temperatura: 11,
-  humedad: 50,
-  viento: 5,
-  precipitaciones: 0,
-};
-
-/* Nosotros necesitamos modelo de datos como este para pasarselo 
-  a la funcion y que nos de un  resultado para saber si podemos escalar o no */
