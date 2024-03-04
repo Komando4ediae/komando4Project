214514d3d8f8f32e368bff327b6d48be55702ec7
Carlos ALvarez <carlosmail1978@gmail.com>
2024-03-04 17:37:53 +0100

    Add docstrings to class

--- climbing-crag-forecast.js
+++ climbing-crag-forecast.js
@@ -1,10 +1,20 @@
 class ClimbingCragForecast {
+  /**
+   * Creates a ClimbingCragForecast object.
+   * @param {string} forecast - The forecast for climbing conditions.
+   * @param {string} location - The location of the climbing crag.
+   */
   constructor(forecast, location) {
     this.forecast = forecast;
     this.location = location;
   }
 
+  /**
+   * Calculates the climbing conditions based on the forecast.
+   */
   calculateConditions() {
-    console.log(`With this${this.forecast} is not posible to climb`);
+    console.log(
+      `With this ${this.forecast} it is not possible to climb at ${this.location}`
+    );
   }
 }
